b8716c0200d490aa8f833f1613105b5b2ca884ea
Joaquin Menchaca <joaquin@dgraph.io>
2020-06-16 19:46:39 -0700

    update to support intuitive tls cli options

--- charts/dgraph/scripts/make_tls_secrets.sh
+++ charts/dgraph/scripts/make_tls_secrets.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+[[ -d ./tls ]] || { echo "Directory './tls' does not exist! Exiting." 2>&1; exit 1; }
+
 ## Given directory ./tls creates a secrets.yaml for use with helm chart
 ## TLS Directory created with `dgraph cert` command, e.g.
 ##  `dgraph cert -n localhost -c dgraphuser`
