54e5f1b4bd614a5994719bf954f5d697f5995a98
Joaquin <joaquin.it@gmail.com>
2020-09-04 11:52:03 -0700

    wip: initial tls, script to generate list of alpha supported in TLS

---
+++ charts/dgraph/scripts/get_alpha_list.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+get_alpha_list() {
+  ## No of replicas in Alpha statefulset
+  REPLICAS=${REPLICAS:-"3"}
+  ## helm release name
+  RELEASE=${RELEASE:-"my-release"}
+  ## namespace used during deployment
+  NAMESPACE=${NAMESPACE:-"default"}
+  ## kuberetes domain, typically cluster.local
+  DOMAIN=${DOMAIN:-"cluster.local"}
+
+  ## Build List
+  for (( IDX=0; IDX<REPLICAS; IDX++ )); do
+    LIST+=("$RELEASE-dgraph-alpha-$IDX.$RELEASE-dgraph-alpha-headless.$NAMESPACE.svc.$DOMAIN")
+  done
+
+  ## Output Comma Seperated Left
+  IFS=,; echo "${LIST[*]}"
+}
+
+get_alpha_list
