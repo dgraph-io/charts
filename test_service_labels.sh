749fb6fb0512ca1237492b92525385dfdd80b672
Joaquin Menchaca <joaquin@dgraph.io>
2022-09-22 16:53:44 -0700

    WIP: tests

--- test_service_labels.sh
+++ test_service_labels.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 
-# PASS
+# TEST 1
 helm template ./charts/dgraph/
-
-# PASS
+# TEST 2
 helm template ./charts/dgraph/ \
   --set "alpha.serviceHeadless.labels.consul\.hashicorp\.com/service-ignore=\"true\"" \
-	| bat -l yaml
-# 
+  | bat -l yaml
+# TEST 3
 helm template ./charts/dgraph/ \
   --set "zero.serviceHeadless.labels.consul\.hashicorp\.com/service-ignore=\"true\"" \
-	| bat -l yaml
-# 
+ | bat -l yaml
+# TEST 4
+helm template ./charts/dgraph/ \
+  --set "zero.service.labels.consul\.hashicorp\.com/service-ignore=\"true\"" \
+  | bat -l yaml
+# TEST 5
 helm template ./charts/dgraph/ \
   --set "alpha.service.labels.consul\.hashicorp\.com/service-ignore=\"true\"" \
-	| bat -l yaml
-# 
+  | bat -l yaml
+# TEST 6
 helm template ./charts/dgraph/ \
-  --set "alpha.service.labels.consul\.hashicorp\.com/service-ignore=\"true\"" \
-	| bat -l yaml
+  --set "ratel.service.labels.consul\.hashicorp\.com/service-ignore=\"true\"" \
+  --set "ratel.enabled=true" \
+  | bat -l yaml
